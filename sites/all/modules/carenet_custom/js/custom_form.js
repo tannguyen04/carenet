9375734bf77b9b58767487b4b36df87ef1222619
HaiBui <haibui.net@gmail.com>
2016-05-21 01:21:54 +0700

    filter field form

--- sites/all/modules/carenet_custom/js/custom_form.js
+++ sites/all/modules/carenet_custom/js/custom_form.js
@@ -1,3 +1,49 @@
 jQuery(document).ready(function($) {
+
 	$('.form-item-date-time input').datetimepicker();
+
+	$('#carenet-custom-provider-action .form-item-reason').hide();
+	$('#carenet-custom-provider-action .form-item-person').hide();
+	$('#carenet-custom-provider-action .form-item-email').hide();
+	$('#carenet-custom-provider-action .form-item-phone').hide();
+	$('#carenet-custom-provider-action .form-item-designation').hide();
+	$('#carenet-custom-provider-action .form-item-date-time').hide();
+	$('#carenet-custom-provider-action .form-item-comment').hide();
+	
+	$('.form-item-status select').change(function(event) {
+		/* Act on the event */
+		if($(this).val() == 4848){
+			$('#carenet-custom-provider-action .form-item-reason').show();
+		}else{
+			$('#carenet-custom-provider-action .form-item-reason').hide();
+		}
+
+		if($(this).val() == 4850){
+			$('#carenet-custom-provider-action .form-item-date-time').show();
+		}else{
+			$('#carenet-custom-provider-action .form-item-date-time').hide();
+		}
+
+		if($(this).val() == 4851){
+			$('#carenet-custom-provider-action .form-item-person').show();
+			$('#carenet-custom-provider-action .form-item-email').show();
+			$('#carenet-custom-provider-action .form-item-phone').show();
+			$('#carenet-custom-provider-action .form-item-designation').show();
+			$('#carenet-custom-provider-action .form-item-date-time').show();
+		}else{
+			$('#carenet-custom-provider-action .form-item-person').hide();
+			$('#carenet-custom-provider-action .form-item-email').hide();
+			$('#carenet-custom-provider-action .form-item-phone').hide();
+			$('#carenet-custom-provider-action .form-item-designation').hide();
+			$('#carenet-custom-provider-action .form-item-date-time').hide();
+		}
+	});
+	
+	$('#carenet-custom-provider-action .form-item-reason').change(function(event) {
+		if($(this).val() == 4918){
+			$('#carenet-custom-provider-action .form-item-comment').show();	
+		}else{
+			$('#carenet-custom-provider-action .form-item-comment').hide();
+		}
+	});
 });
