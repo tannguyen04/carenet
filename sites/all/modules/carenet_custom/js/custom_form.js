9f30b6754186a1fe990687adee843ea2d45482b8
HaiBui <haibui.net@gmail.com>
2016-05-21 23:46:29 +0700

    fix form

--- sites/all/modules/carenet_custom/js/custom_form.js
+++ sites/all/modules/carenet_custom/js/custom_form.js
@@ -7,10 +7,6 @@ jQuery(document).ready(function($) {
 	
 	function hide_all(){
 		$('#carenet-custom-provider-action .form-item-reason').hide();
-		$('#carenet-custom-provider-action .form-item-person').hide();
-		$('#carenet-custom-provider-action .form-item-email').hide();
-		$('#carenet-custom-provider-action .form-item-phone').hide();
-		$('#carenet-custom-provider-action .form-item-designation').hide();
 		$('#carenet-custom-provider-action .form-item-date-time').hide();
 		$('#carenet-custom-provider-action .form-item-comment').hide();
 		$('#carenet-custom-provider-action .form-item-email-sent').hide();
