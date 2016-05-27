14efe0e4b7ae63efabb79402a95ec101a11b07bc
HaiBui <haibui.net@gmail.com>
2016-05-27 22:21:22 +0700

    fix rule form

--- sites/all/modules/carenet_custom/js/custom_form.js
+++ sites/all/modules/carenet_custom/js/custom_form.js
@@ -54,6 +54,12 @@ jQuery(document).ready(function($) {
 			$('#carenet-custom-provider-action .form-item-reason').show();
 		}else if($(this).val() == 4850){
 			hide_all();
+			$('#carenet-custom-provider-action .form-item-person').hide();
+			$('#carenet-custom-provider-action .form-item-email').hide();
+			$('#carenet-custom-provider-action .form-item-phone').hide();
+			$('#carenet-custom-provider-action .form-item-designation').hide();
+			$('#carenet-custom-provider-action .form-item-title').hide();
+		
 			$('#carenet-custom-provider-action .form-item-date-time').show();
 		}else if($(this).val() == 4851){
 			hide_all();
