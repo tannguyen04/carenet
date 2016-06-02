67bb141b4a49e29b5bb88d0f28b25375a6167083
Hai Bui <haibqit@gmail.com>
2016-06-03 01:02:16 +0700

    fix form add provider

--- sites/all/modules/carenet_custom/js/custom_add_provider.js
+++ sites/all/modules/carenet_custom/js/custom_add_provider.js
@@ -1,3 +1,18 @@
 jQuery(document).ready(function($) {
-	
+	$('.form-item-field-pv-state-und select').change(function(event) {
+		$('.form-item-field-pv-city-und select').prop('disabled', true);
+		$.get( "/get_data_city?state="+$(this).val(), function( data ) {
+      $('.form-item-field-pv-city-und select').html(data);
+      $('.form-item-field-pv-city-und select').prop('disabled', false);
+    });
+	});
+
+	$('.form-item-field-pv-city-und select').change(function(event) {
+		$('.form-item-field-pv-county-und select').prop('disabled', true);
+		$.get( "/get_data_county?city="+$(this).val(), function( data ) {
+      $('.form-item-field-pv-county-und select').html(data);
+      $('.form-item-field-pv-county-und select').prop('disabled', false);
+    });
+	});
+
 });
