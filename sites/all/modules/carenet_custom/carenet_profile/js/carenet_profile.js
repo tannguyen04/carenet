9077586ea4bda9c183b33ddaadea7a7633442a0d
Trang Lee <trangunghoa@gmail.com>
2016-06-07 02:37:14 +0700

    add carenet_profile module

--- sites/all/modules/carenet_custom/carenet_profile/js/carenet_profile.js
+++ sites/all/modules/carenet_custom/carenet_profile/js/carenet_profile.js
@@ -3,7 +3,8 @@
  */
 (function ($) {
 console.log('test');
-    if (document.getElementById('edit-profile-main-field-prostaff-license-und-3').checked) {
+    //if (document.getElementById('edit-profile-main-field-prostaff-license-und-3').checked) {
+    if (document.getElementById('edit-profile-main-field-prostaff-license-und-3').is(':checked')) {
         console.log("checked");
     } else {
         $('#edit-profile-main-field-prostaff-license-other').hide();
