9ddb6a7b36505356a332b90ff06f48e8280cfd06
Trang Lee <trangunghoa@gmail.com>
2016-06-07 20:22:47 +0700

    remove code

--- sites/all/modules/carenet_custom/carenet_profile/js/carenet_profile.js
+++ sites/all/modules/carenet_custom/carenet_profile/js/carenet_profile.js
@@ -4,14 +4,16 @@
 (function ($) {
 console.log('test');
     $('#edit-sign').click(function(e){
+        console.log('edit-sign');
         e.preventDefault();
         $('#edit-profile-main-field-profile-sign-und-0-value').val(1);
         document.getElementById("user-profile-form").submit();
     });
+    /*
     //if (document.getElementById('edit-profile-main-field-prostaff-license-und-3').checked) {
     if (document.getElementById('edit-profile-main-field-prostaff-license-und-3').is(':checked')) {
         console.log("checked");
     } else {
         $('#edit-profile-main-field-prostaff-license-other').hide();
-    }
+    }*/
 }(jQuery));
